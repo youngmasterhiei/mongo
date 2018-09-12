9d484ecbb5708fedc879b911f6bdbf407f0d3e19
james I <james.illius@yahoo.com>
2018-09-11 20:01:12 -0400

    working display of articles

--- public/js/index.js
+++ public/js/index.js
@@ -18,9 +18,9 @@ $(document).ready(function () {
     var displayArticles = function () {
         API.getArticles().then(function (data) {
             console.log(data);
-            // for (var i = 0; i < data.length; i++) {
-            //           $("#articles").append("<h1 class='title' data-id='" + data[i]._id + "'>" + data[i].title + "</h1><p class='summary'>" + data[i].summary + "</p> <a href='" + data[i].link + "'>" + data[i].title + "</a>");
-            //         }
+            for (var i = 0; i < data.length; i++) {
+                      $("#articles").append("<h1 class='title' data-id='" + data[i]._id + "'>" + data[i].title + "</h1><p class='summary'>" + data[i].summary + "</p> <a href='" + data[i].link +  "'target='blank' >'" + data[i].title + "</a>");
+                    }
         }); 
         
     };
